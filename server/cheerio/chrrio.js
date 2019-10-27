6387480ccac7f5ad4686d9f4f7e8a25f0b062988
OctoberCity <1730653658@qq.com>
2019-10-27 19:19:14 +0800

    重构删除mongoose 添加sequelize

--- server/cheerio/chrrio.js
+++ server/cheerio/chrrio.js
@@ -1,6 +1,8 @@
 const EventEmitter = require('events');
 const Redis = require("ioredis");
 const Sequelize = require('sequelize');
+const superagent = require('superagent');
+const unique = require('node-uuid');
 class Chrrio extends EventEmitter {
     constructor(redis, sequelize) {
         super();
@@ -21,11 +23,12 @@ class Chrrio extends EventEmitter {
                 sequelize.database,
                 sequelize.username,
                 sequelize.password, {
-                    dialect: sequelize.dialect,
-                    host: sequelize.host,
-                    port: sequelize.port, 
-                });
+                dialect: sequelize.dialect,
+                host: sequelize.host,
+                port: sequelize.port,
+            });
         }
+
 
         /*** 
          *  register redis===>
@@ -38,106 +41,149 @@ class Chrrio extends EventEmitter {
         this.on('forbidProxy', (ip) => {
 
         });
+    }
 
-        // 取出可用代理
-        async function pullProxy() {}
-
-        // 取出正在使用的代理
-        async function pullUseingProxy() {
-
-        }
-
-        //初始化/更新全部代理
-        async function initAllProxy() {}
-
-
-        /***
-         * 关于查询条件的处理
-         * https://www.zhipin.com/common/data/city.json 城市数据
-         * https://www.zhipin.com/common/data/position.json 职位数据
-         * 
-         */
-
-        // 添加需要爬取的条件至队列
-        async function addWorkToQueue() {
-
-        }
-        // 获取需要爬取的条件至队列
-        async function getWorkToQueue() {}
-        // 查询某条件下是否还有任务
-        async function checkfinish() {
-            
-        }
-
-
-
-
-
-        /***
-         * 业务操作
-         * 
-         */
-
-        // 插入更新position 至数据库
-        async function updatePos() {
-
-        }
-        // 插入更新city 至数据库
-        async function updateCity() {
-
-        }
-
-
-        // 插入一页的数据至数据库
-        async function insertOnePageInfo(sres, city, position) {
-            const $ = cheerio.load(sres.text);
-            const items = [];
-            $('.job-list li').each(function (idx, element) {
-                const $element = $(element);
-                const infoObj = {};
-                //TODO数据还没有详细分开
-                $element.find($('.info-company   em')).text(','); //添加分隔符将em标签变成','
-                $element.find($('.info-primary   em')).text(','); //添加分隔符将em标签变成','
-                $element.find($('.info-publis   em')).text(','); //添加分隔符将em标签变成','
-                infoObj.name = $element.find($('.job-title')).text();
-                infoObj.slary = $element.find($('.red')).text();
-                infoObj.companyname = $element.find($('.info-company .name  a')).text();
-                infoObj.companyInfo = $element.find($('.info-company   p')).text().split(',');
-                infoObj.jobInfo = $element.find($('.info-primary  p')).text().split(',');
-                infoObj.hrinfo = $element.find('.info-publis  h3 ').text().split(',');
-                infoObj.hrinfo.push($element.find('.info-publis img').attr('src'));
-                items.push(infoObj);
-            });
-            // 判断是不是有下一页
-            const noNext = $('.page .next').hasClass("disabled");
-            const current = $('.page .cur').html();
-            // 批量插入数据 
-            if (!noNext) {
-                //  `https://www.zhipin.com/c${city}-p${position}/?page=${++current}&ka=page-${++current}`
-                this.addWorkToQueue();
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // 取出可用代理
+    pullProxy() {
 
     }
+
+    // 取出正在使用的代理
+    pullUseingProxy() {
+
+    }
+
+    //初始化/更新全部代理
+    initAllProxy() { }
+
+
+    /***
+     * 关于查询条件的处理
+     * https://www.zhipin.com/common/data/city.json 城市数据
+     * https://www.zhipin.com/common/data/position.json 职位数据
+     * 
+     */
+    // 获得省级集合中是否还有数据，重启时确认继续爬取
+    findRedisProvinceWork() {
+
+    }
+    // 移除省级集合某个省级并销毁省级队列
+    removeProvinceWork(province) { }
+
+    // 获取某个省级中市级单位是否还有多少
+    findRedisCityWork(province) {
+
+    }
+
+
+
+
+
+
+    /***
+     * 业务操作
+     * 
+     */
+
+    // 插入更新position 至数据库
+    updatePos() {
+        superagent.get('https://www.zhipin.com/common/data/position.json').end((err, res) => {
+            const data = res.body.data;
+            const allPosition = [];
+            function fixData(parent, arr) {
+                for (let i = 0; i < arr.length; i++) {
+                    const id = unique();
+                    allPosition.push({
+                        id,
+                        parents: parent,
+                        code: arr[i].code,
+                        name: arr[i].name
+                    });  
+                    if (arr[i].subLevelModelList) { 
+                        fixData(id, arr[i].subLevelModelList);
+                    }
+                }
+            }
+            fixData(0, data); 
+            // 插入数据库
+        })
+    }
+    // 插入更新city 至数据库
+    async   updateCity() {
+        superagent.get('https://www.zhipin.com/common/data/city.json').end((err, res) => {
+            const data = res.body.data;
+            const allPosition = [];
+            function fixData(parent, arr) {
+                for (let i = 0; i < arr.length; i++) {
+                    const id = unique();
+                    allPosition.push({
+                        id,
+                        parents: parent,
+                        code: arr[i].code,
+                        name: arr[i].name
+                    });  
+                    if (arr[i].subLevelModelList) { 
+                        fixData(id, arr[i].subLevelModelList);
+                    }
+                }
+            }
+            fixData(0, data); 
+            // 插入数据库
+        })
+    }
+
+
+    // 插入一页的数据至数据库
+    insertOnePageInfo(sres, city, position) {
+        const $ = cheerio.load(sres.text);
+        const items = [];
+        $('.job-list li').each(function (idx, element) {
+            const $element = $(element);
+            const infoObj = {};
+            //TODO数据还没有详细分开
+            $element.find($('.info-company   em')).text(','); //添加分隔符将em标签变成','
+            $element.find($('.info-primary   em')).text(','); //添加分隔符将em标签变成','
+            $element.find($('.info-publis   em')).text(','); //添加分隔符将em标签变成','
+            infoObj.name = $element.find($('.job-title')).text();
+            infoObj.slary = $element.find($('.red')).text();
+            infoObj.companyname = $element.find($('.info-company .name  a')).text();
+            infoObj.companyInfo = $element.find($('.info-company   p')).text().split(',');
+            infoObj.jobInfo = $element.find($('.info-primary  p')).text().split(',');
+            infoObj.hrinfo = $element.find('.info-publis  h3 ').text().split(',');
+            infoObj.hrinfo.push($element.find('.info-publis img').attr('src'));
+            items.push(infoObj);
+        });
+        // 判断是不是有下一页
+        const noNext = $('.page .next').hasClass("disabled");
+        const current = $('.page .cur').html();
+        //TODO批量插入数据 
+
+        if (noNext) {// 如果没有下一页
+            //  `https://www.zhipin.com/c${city}-p${position}/?page=${++current}&ka=page-${++current}`
+            this.addWorkToQueue();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- server/cheerio/index.js
+++ server/cheerio/index.js
@@ -1,35 +1,36 @@
 const Chrrio = require('./chrrio');
 const redisConf = {
-    host: '192.168.17.180',
+    host: '192.168.31.10',
     port: '6379'
 }
 const mysqlConf = {
     database: 'chrrio',
     username: 'root',
     password: '123456',
-    host: '192.168.17.180',
+    host: '192.168.31.10',
     port: '3306',
     dialect: 'mysql'
 }
 const chrrio = new Chrrio(redisConf, mysqlConf);
+chrrio.updatePos();
 let isEnd = true;
-process.on('message', (info) => {
-    switch (info.type) {
-        case 1: // 工作做完
-            isEnd = false;
-            break;
-    }
-})
-while (isEnd) {
-    work();
-}
+// process.on('message', (info) => {
+//     switch (info.type) {
+//         case 1: // 工作做完
+//             isEnd = false;
+//             break;
+//     }
+// })
+// while (isEnd) {
+//     work();
+// }
 
 
 
-function work() {
+// function work() {
  
 
-}
+// }
 
 
 
